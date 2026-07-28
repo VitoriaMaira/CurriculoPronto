95899bb1b1ebebf6d99ec96c87024ac776703955
vitoria <vitoria.maira@souunit.com.br>
2026-07-28 16:37:30 -0300

    chore: configura a execução da API e do Worker pelo AppHost

--- src/ResumeForge.AppHost/AppHost.cs
+++ src/ResumeForge.AppHost/AppHost.cs
@@ -1,3 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.CurriculoPronto_Api>("api");
+builder.AddProject<Projects.CurriculoPronto_Worker>("worker");
+
 builder.Build().Run();
